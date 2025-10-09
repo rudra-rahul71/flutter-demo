9b7a8b47b8827cfad2c5bab04d30d0cf7fd9f840
Rahul Rudra <rahul@appreeze.com>
2025-10-09 13:16:38 -0400

    shared preferences

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter_tester/features/profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Finance Tracker'),
         centerTitle: true,
-        // Now, Theme.of(context) will find the redAccent-based theme.
         backgroundColor: Theme.of(context).colorScheme.onPrimary,
       ),
       drawer: const Drawer(),
@@ -20,12 +27,73 @@ class HomePage extends StatelessWidget {
           NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
           NavigationDestination(icon: Icon(Icons.account_circle), label: 'Profile')
         ],
-        onDestinationSelected: (value) => {
+        onDestinationSelected: (value) async => {
           if(value == 1) {
-            Navigator.push(context, MaterialPageRoute(builder: (_) => ProfilePage()))
+            await Navigator.push(context, MaterialPageRoute(builder: (_) => ProfilePage())),
+            _loadPrefs()
           }
         },
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _isLoading ? [CircularProgressIndicator()] :
+                    accessToken != null ? [Text(accessToken!)] : [Text("Go to Profile and set up Plaid integration!")],
+        )
+      )
     );
+  }
+
+  String? accessToken;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPrefs();
+  }
+
+  Future<void> _loadPrefs() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    accessToken = prefs.getString('accessToken');
+
+    if (accessToken != null) {
+      _checkAccountBalance();
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _checkAccountBalance() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final url = Uri.parse('http://10.0.2.2:8080/balance');
+
+    final response = await http.get(url);
+
+    try {
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print(data['accounts']);
+      } else {
+        throw Exception('Failed to check account balance');
+      }
+    } catch (e) {
+      setState(() {
+        // linkToken = 'Error: $e';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 }
--- lib/features/profile.dart
+++ lib/features/profile.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:plaid_flutter/plaid_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -15,44 +16,46 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: const Text('Profile'),
-      centerTitle: true,
-      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-    ),
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.onPrimary,
+      ),
       body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: _isLoading ? null : _initPlaidIntegration,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                  foregroundColor: Theme.of(context).colorScheme.inverseSurface,
-                ),
-                child: const Text('Init Account'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _isLoading ? CircularProgressIndicator() :
+            accessToken != null ? Text("Profile Setup Complete") : ElevatedButton(
+              onPressed: _initPlaidIntegration,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                foregroundColor: Theme.of(context).colorScheme.inverseSurface,
               ),
-              ElevatedButton(
-                onPressed: _isLoading ? null : _checkAccountBalance,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                  foregroundColor: Theme.of(context).colorScheme.inverseSurface,
-                ),
-                child: const Text('Check Balance'),
-              ),
-              const SizedBox(height: 20),
-              // Display either a loading indicator or the response text.
-              _isLoading ? const CircularProgressIndicator()
-                : linkToken != null ? Text(linkToken!) : Text("Load link token")
-            ],
-          ),
+              child: const Text('Integrate with Plaid'),
+            ),
+          ],
         ),
+      ),
     );
   }
   // A string to hold the data fetched from the API.
   // It defaults to a prompt for the user.
-  String? linkToken;
+  String? accessToken;
   bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPrefs();
+  }
+
+  Future<void> _loadPrefs() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      accessToken = prefs.getString('accessToken');
+    });
+  }
 
   Future<void> _initPlaidIntegration() async {
     setState(() {
@@ -74,11 +77,21 @@ class _ProfilePageState extends State<ProfilePage> {
           final publicToken = event.toJson()['publicToken'];
           final url2 = Uri.parse('http://10.0.2.2:8080/create/$publicToken');
 
-          await http.get(url2);
+          final response2 = await http.get(url2);
+
+          if (response2.statusCode == 200) {
+            final data2 = json.decode(response2.body);
+            if(data2 != null && data2['access_token'] != null) {
+              final SharedPreferences prefs = await SharedPreferences.getInstance();
+              await prefs.setString('accessToken', data2['access_token']);
+              setState(() {
+                accessToken = data2['access_token'];
+              });
+            }
+          }
         });
         PlaidLink.open();
         setState(() {
-          linkToken = data['link_token'];
           // _apiResponse = 'Title: ${data['title']}\n\nBody: ${data['body']}';
         });
       } else {
@@ -86,34 +99,7 @@ class _ProfilePageState extends State<ProfilePage> {
       }
     } catch (e) {
       setState(() {
-        linkToken = 'Error: $e';
-      });
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
 
-  Future<void> _checkAccountBalance() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    final url = Uri.parse('http://10.0.2.2:8080/balance');
-
-    final response = await http.get(url);
-
-    try {
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        print(data['accounts']);
-      } else {
-        throw Exception('Failed to check account balance');
-      }
-    } catch (e) {
-      setState(() {
-        // linkToken = 'Error: $e';
       });
     } finally {
       setState(() {
